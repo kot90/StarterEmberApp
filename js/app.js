dad57c1d7827ca28634fb7c0e48a4fb489cf94fa
John HB <jhbco@hotmail.com>
2014-03-30 14:22:58 -0500

    second

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 App = Ember.Application.create({});
 
+
+App.Router.map(function(){
+	this.resource("about");
+});
+
 App.IndexRoute = Ember.Route.extend({
 	setupController: function(controller){
 		controller.set('content',['red','yellow','blue']);

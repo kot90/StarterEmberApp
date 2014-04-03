ad9368480f8c495f02bc2419046730189e283885
John HB <jhbco@hotmail.com>
2014-04-02 19:37:29 -0500

    working demo

--- js/app.js
+++ js/app.js
@@ -6,7 +6,47 @@ App.Router.map(function(){
 });
 
 App.IndexRoute = Ember.Route.extend({
-	setupController: function(controller){
-		controller.set('content',['red','yellow','blue']);
+	model:function(){
+		return App.RedditLink.findAll('aww');
+	}
+});
+
+App.IndexController = Ember.ObjectController.extend({
+	subredditHeader:"aww",
+	loadList:function(){
+		//grab the value from the input field
+		var value = this.get("subreddit");
+
+		if(value){
+			this.set('subredditHeader',value);
+			this.set('model',App.RedditLink.findAll(value));
+			//clear out input field
+			this.set("subreddit","");
+		}
+
+
+
+	}
+});
+
+App.RedditLink= Ember.Object.extend({
+
+	thumbnailUrl:function(){
+		var thumbnail = this.get('thumbnail');
+		return (thumbnail === 'default') ? null:thumbnail;
+	}.property('thumbnail')
+});
+
+App.RedditLink.reopenClass({
+
+	findAll:function(subreddit){
+		var links =[];
+
+		$.getJSON("http://www.reddit.com/r/" + subreddit + '/.json?jsonp=?').then(function(response){
+			response.data.children.forEach(function(child){
+				links.pushObject(App.RedditLink.create(child.data));
+			});
+		});
+		return links;
 	}
 });
